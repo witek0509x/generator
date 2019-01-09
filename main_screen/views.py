df9bfb0c255db9695c217df319aa62f2071c5a29
Wojciech Drochomirecki <witek.droch@gmail.com>
2019-01-09 11:36:43 +0100

    added bootstrap template

--- main_screen/views.py
+++ main_screen/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    template = loader.get_template('main_page/index.html')
+    contex = {}
+    return HttpResponse(template.render(contex, request))
